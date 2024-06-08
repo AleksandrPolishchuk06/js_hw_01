bfaba1f9b5b4c1c5d99d895495d24ea0454d7581
Aleksandr0607 <162420175+Aleksandr0607@users.noreply.github.com>
2024-06-08 22:08:45 +0300

    first commit

---
+++ js/js_task_05.js
@@ -0,0 +1,13 @@
+// let total = 0;
+// let input;
+
+// for (let total = 0; input !== null; input += total) {
+//   let input = prompt(`введіть число, ${input}`);
+// }
+
+const target = 0;
+
+let sum = prompt("VEDITM", "");
+for (let i = target; i === 100; i += sum) {
+  console.log(sum);
+}
